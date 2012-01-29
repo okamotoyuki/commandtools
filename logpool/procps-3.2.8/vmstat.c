44ae5940460cc3b7d9437352da1b8b02695d74b7
Yuki Okamoto <Okamoto.Yuki.0130@gmail.com>
2012-01-30 02:01:54 +0900

    support L_RECORD macro.

--- logpool/procps-3.2.8/vmstat.c
+++ logpool/procps-3.2.8/vmstat.c
@@ -180,7 +180,7 @@ static void new_header(void){
 			"us","sy","id","wa"
 			);
 	for(; counter < 2; counter++) {
-		ltrace_record(ltrace, prefix,
+		L_RECORD(
 				LOG_s("", line[counter]),
 				LOG_END
 				);
@@ -263,7 +263,7 @@ static void new_format(void) {
 			(unsigned)( (100*diow                    + divo2) / Div ) /* ,
 																																	 (unsigned)( (100*dstl                    + divo2) / Div ) */
 			);
-	ltrace_record(ltrace, prefix,
+	L_RECORD(
 			LOG_s("", line[0]),
 			LOG_END
 			);
@@ -333,7 +333,7 @@ static void new_format(void) {
 				(unsigned)( (100*diow+divo2)/Div )/*, //wa
 																						(unsigned)( (100*dstl+divo2)/Div )  //st  */
 				);
-		ltrace_record(ltrace, prefix,
+		L_RECORD(
 				LOG_s("", line[1]);
 				LOG_END
 				);
@@ -346,7 +346,7 @@ static void diskpartition_header(const char *partition_name){
 //	printf("%-10s %10s %10s %10s %10s\n",partition_name, "reads  ", "read sectors", "writes   ", "requested writes");
 	char line[LINELEN];
 	snprintf(line, LINELEN, "%-10s %10s %10s %10s %10s\n",partition_name, "reads  ", "read sectors", "writes   ", "requested writes");
-	ltrace_record(ltrace, prefix,
+	L_RECORD(
 			LOG_s("", line),
 			LOG_END
 			);
@@ -384,7 +384,7 @@ static int diskpartition_format(const char* partition_name){
 //			current_partition->reads,current_partition->reads_sectors,current_partition->writes,current_partition->requested_writes);
 	snprintf (line[0], LINELEN, format,
 			current_partition->reads,current_partition->reads_sectors,current_partition->writes,current_partition->requested_writes);
-	ltrace_record(ltrace, prefix,
+	L_RECORD(
 			LOG_s("", line[0]),
 			LOG_END
 			);
@@ -409,7 +409,7 @@ static int diskpartition_format(const char* partition_name){
 //				current_partition->reads,current_partition->reads_sectors,current_partition->writes,current_partition->requested_writes);
 		snprintf (line[1], LINELEN, format,
 				current_partition->reads,current_partition->reads_sectors,current_partition->writes,current_partition->requested_writes);
-		ltrace_record(ltrace, prefix,
+		L_RECORD(
 				LOG_s("", line[1]),
 				LOG_END
 				);
@@ -426,7 +426,7 @@ static void diskheader(void){
 	//	printf("disk- ------------reads------------ ------------writes----------- -----IO------\n");
 	char line[2][LINELEN];
 	snprintf(line[0], LINELEN, "disk- ------------reads------------ ------------writes----------- -----IO------\n");
-	ltrace_record(ltrace, prefix,
+	L_RECORD(
 			LOG_s("", line[0]),
 			LOG_END
 			);
@@ -434,7 +434,7 @@ static void diskheader(void){
 //			" ", "total", "merged","sectors","ms","total","merged","sectors","ms","cur","sec");
 	snprintf(line[1], LINELEN, "%5s %6s %6s %7s %7s %6s %6s %7s %7s %6s %6s\n",
 			" ", "total", "merged","sectors","ms","total","merged","sectors","ms","cur","sec");
-	ltrace_record(ltrace, prefix,
+	L_RECORD(
 			LOG_s("", line[1]),
 			LOG_END
 			);
@@ -482,7 +482,7 @@ static void diskformat(void){
 					disks[k].milli_spent_IO?disks[k].milli_spent_IO/1000:0/*,
 																																	disks[i].weighted_milli_spent_IO/1000*/
 					);
-			ltrace_record(ltrace, prefix,
+			L_RECORD(
 					LOG_s("", line[0]),
 					LOG_END
 					);
@@ -523,7 +523,7 @@ static void diskformat(void){
 						disks[i].milli_spent_IO?disks[i].milli_spent_IO/1000:0/*,
 																																		disks[i].weighted_milli_spent_IO/1000*/
 						);
-				ltrace_record(ltrace, prefix,
+				L_RECORD(
 						LOG_s("", line[1]),
 						LOG_END
 						);
@@ -544,7 +544,7 @@ static void slabheader(void){
 //	printf("%-24s %6s %6s %6s %6s\n","Cache","Num", "Total", "Size", "Pages");
 	char line[LINELEN];
 	snprintf(line, LINELEN, "%-24s %6s %6s %6s %6s\n","Cache","Num", "Total", "Size", "Pages");
-	ltrace_record(ltrace, prefix,
+	L_RECORD(
 			LOG_s("", line),
 			LOG_END
 			);
@@ -581,7 +581,7 @@ static void slabformat (void){
 				slabs[k].objsize,
 				slabs[k].objperslab
 				);
-		ltrace_record(ltrace, prefix,
+		L_RECORD(
 				LOG_s("", line[0]),
 				LOG_END
 				);
@@ -606,7 +606,7 @@ static void slabformat (void){
 					slabs[i].objsize,
 					slabs[i].objperslab
 					);
-			ltrace_record(ltrace, prefix,
+			L_RECORD(
 					LOG_s("", line[1]),
 					LOG_END
 					);
@@ -674,7 +674,7 @@ static void disksum_format(void) {
 		snprintf(line[9], LINELEN, "%13lu inprogress IO\n",inprogress_IO);
 		snprintf(line[10], LINELEN, "%13lu milli spent IO\n",milli_spent_IO);
 		for(; counter < 11; counter++) {
-			ltrace_record(ltrace, prefix,
+			L_RECORD(
 					LOG_s("", line[counter]),
 					LOG_END
 					);
@@ -757,7 +757,7 @@ static void sum_format(void) {
 	snprintf(line[24], LINELEN, "%13u boot time\n", btime);
 	snprintf(line[25], LINELEN, "%13u forks\n", processes);
 	for(; counter < 26; counter++) {
-		ltrace_record(ltrace, prefix,
+		L_RECORD(
 				LOG_s("", line[counter]),
 				LOG_END
 				);
@@ -782,7 +782,7 @@ static void fork_format(void) {
 
 //	printf("%13u forks\n", processes);
 	snprintf(line, LINELEN, "%13u forks\n", processes);
-	ltrace_record(ltrace, prefix,
+	L_RECORD(
 			LOG_s("", line),
 			LOG_END
 			);
