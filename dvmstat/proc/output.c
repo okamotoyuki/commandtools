5f216c7891ad1435e33747de1c8ede535cfee3b9
Okamoto Yuki <yuki@Okamotos-MacBook-Pro.local>
2012-06-25 11:12:33 +0900

    add timestamp & context info

--- dvmstat/proc/output.c
+++ dvmstat/proc/output.c
@@ -9,9 +9,6 @@ logpool_t *lp;
 void *lp_args;
 int context = 0;
 
-#define OPT_LOGPOOL_LEN 9
-#define MAX_LOGPOOLIP_LEN 32
-
 logpool_t *logpool_open_trace32(logpool_t *parent, char *host, int port)
 {
 	struct logpool_param_stream param = {32, 2048};
@@ -21,8 +18,13 @@ logpool_t *logpool_open_trace32(logpool_t *parent, char *host, int port)
 	return logpool_open(parent, &STREAM_API, (struct logpool_param*) &param);
 }
 
+#define OPT_LOGPOOL_LEN 9
+#define OPT_CONTEXT_LEN 9
+#define MAX_LOGPOOLIP_LEN 32
+
 int configureLogPool(char *arg) {
 	char opt_logpool[] = "-logpool=";
+	char opt_context[] = "-context=";
 	char logpoolip[MAX_LOGPOOLIP_LEN];
 
 	if(arg == NULL) {
@@ -34,6 +36,11 @@ int configureLogPool(char *arg) {
 			arg += OPT_LOGPOOL_LEN;
 			strncpy(logpoolip, arg, strlen(arg) + 1);
 			if(!lp) lp = logpool_open_trace32(NULL, logpoolip, 14801);
+			return 0;
+		}
+		else if(strncmp(arg, opt_context, OPT_CONTEXT_LEN) == 0) {
+			arg += OPT_CONTEXT_LEN;
+			context = atoi(arg);
 			return 0;
 		}
 	}
--- dvmstat/proc/output.h
+++ dvmstat/proc/output.h
@@ -7,7 +7,6 @@
 #define LOG_u   2
 
 #define KEYVALUE_u(K,V)    LOG_u, (K), ((uintptr_t)V)
-#define KEYVALUE_s(K,V)    LOG_s, (K), (V)
 
 extern logpool_t *lp;
 extern void *lp_args;
@@ -15,8 +14,15 @@ extern int context;
 
 extern int configureLogPool(char *arg);
 
+static int getTime(void)
+{
+	struct timeval t;
+	gettimeofday(&t, NULL);
+	return (int)((int)t.tv_sec * 1000 * 1000 + (int)t.tv_usec);
+}
+
 #define L_RECORD(...) \
 	logpool_record(lp, &lp_args, LOG_NOTICE, "dvmstat", \
+			KEYVALUE_u("time", getTime()), \
+			KEYVALUE_u("context", context), \
 			__VA_ARGS__);
-
-//	KEYVALUE_s("context", context), \
